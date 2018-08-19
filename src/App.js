5d4675c204fd20c1ee19a8cab4f2c59d67683ada
drewkiimon <andrew.r.pagan@gmail.com>
2018-08-18 20:17:26 -0700

    Logic finished

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ const orientation = {
   EAST: { x: 1, y: 0 }
 };
 
+// Used to get direction for output
+const direction = {
+  x: {
+    "0": {
+      y: {
+        "1": "NORTH",
+        "-1": "SOUTH"
+      }
+    },
+    "1": {
+      y: {
+        "0": "EAST"
+      }
+    },
+    "-1": {
+      y: {
+        "0": "WEST"
+      }
+    }
+  }
+};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +45,10 @@ class App extends Component {
   }
 
   componentDidUpdate(prevState) {
-    if (this.state.location !== null) {
+    if (
+      this.state.location !== null &&
+      this.state.location !== prevState.location
+    ) {
       console.log("component", this.state);
     }
   }
@@ -38,8 +63,8 @@ class App extends Component {
         console.log("PLACE");
         // Make sure we have all 4 arguments
         if (inputLine.length === 4) {
-          const x = inputLine[1],
-            y = inputLine[2],
+          const x = parseInt(inputLine[1]),
+            y = parseInt(inputLine[2]),
             f = inputLine[3];
           // Is PLACE location valid?
           if (x > -1 && x < 6 && y > -1 && y < 6) {
@@ -55,14 +80,40 @@ class App extends Component {
         }
       }
       // If the robot is not on the board, no actions can be taken
-      if (this.place) {
+      if (this.state.placed) {
         if (command === "MOVE") {
           console.log("MOVE");
+          const moveX = this.state.facing.x;
+          const moveY = this.state.facing.y;
+          // Make sure the robot won't fall off the table
+          const nextX = this.state.location.x + moveX;
+          const nextY = this.state.location.y + moveY;
+          if (nextX > -1 && nextX < 6 && nextY > -1 && nextY < 6) {
+            // Good to go
+            this.setState({ location: { x: nextX, y: nextY } });
+          } else {
+            console.log("Not a valid move");
+          }
         } else if (command === "LEFT") {
           console.log("LEFT");
+          const x = this.state.facing.x;
+          const y = this.state.facing.y;
+          this.setState({ facing: { x: -y, y: x } });
         } else if (command === "RIGHT") {
           console.log("RIGHT");
+          const x = this.state.facing.x;
+          const y = this.state.facing.y;
+          this.setState({ facing: { x: y, y: -x } });
         } else if (command === "REPORT") {
+          // Need to print to screen somewhere
+          const location = this.state.location;
+          console.log(
+            `Output: ${location.x},${location.y},${
+              direction.x[this.state.facing.x.toString()].y[
+                this.state.facing.y.toString()
+              ]
+            }`
+          );
           this.setState({
             location: null,
             facing: { x: 0, y: 0 },
