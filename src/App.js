00fb5500fa434273e70f27741feb9a80d8b16371
drewkiimon <andrew.r.pagan@gmail.com>
2018-08-18 21:18:21 -0700

    Finished. Now to clean up code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import ActionView from "./components/ActionView";
+import "./App.css";
 
 const orientation = {
   NORTH: { x: 0, y: 1 },
@@ -37,20 +39,23 @@ class App extends Component {
       value: "",
       location: null,
       facing: { x: 0, y: 0 },
-      placed: false
+      placed: false,
+      actions: []
     };
 
     this.handlePress = this.handlePress.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
-  componentDidUpdate(prevState) {
-    if (
-      this.state.location !== null &&
-      this.state.location !== prevState.location
-    ) {
-      console.log("component", this.state);
-    }
+  handleReset(event) {
+    this.setState({
+      value: "",
+      location: null,
+      facing: { x: 0, y: 0 },
+      placed: false,
+      actions: []
+    });
   }
 
   handlePress(event) {
@@ -59,67 +64,63 @@ class App extends Component {
       const inputLine = this.state.value.split(/[\s,]+/);
       var command = inputLine[0];
       // The starting command
+      // Can be done more than once
       if (command === "PLACE") {
-        console.log("PLACE");
         // Make sure we have all 4 arguments
         if (inputLine.length === 4) {
-          const x = parseInt(inputLine[1]),
-            y = parseInt(inputLine[2]),
+          const x = parseInt(inputLine[1], 10),
+            y = parseInt(inputLine[2], 10),
             f = inputLine[3];
           // Is PLACE location valid?
-          if (x > -1 && x < 6 && y > -1 && y < 6) {
-            const facing = orientation[f];
-            this.setState({ location: { x, y }, facing, placed: true });
-          } else {
-            console.log("Not on board");
+          const facing = orientation[f];
+          if (x > -1 && x < 5 && y > -1 && y < 5 && facing) {
+            this.setState({
+              location: { x, y },
+              facing,
+              placed: true,
+              actions: [...this.state.actions, `PLACE ${x},${y},${f}`]
+            });
           }
-        }
-        // Need to alert user they have an incorrect line
-        else {
-          console.log("Place is wrong");
         }
       }
       // If the robot is not on the board, no actions can be taken
       if (this.state.placed) {
         if (command === "MOVE") {
-          console.log("MOVE");
           const moveX = this.state.facing.x;
           const moveY = this.state.facing.y;
           // Make sure the robot won't fall off the table
           const nextX = this.state.location.x + moveX;
           const nextY = this.state.location.y + moveY;
-          if (nextX > -1 && nextX < 6 && nextY > -1 && nextY < 6) {
+          if (nextX > -1 && nextX < 5 && nextY > -1 && nextY < 5) {
             // Good to go
-            this.setState({ location: { x: nextX, y: nextY } });
-          } else {
-            console.log("Not a valid move");
+            this.setState({
+              location: { x: nextX, y: nextY },
+              actions: [...this.state.actions, "MOVE"]
+            });
           }
         } else if (command === "LEFT") {
-          console.log("LEFT");
           const x = this.state.facing.x;
           const y = this.state.facing.y;
-          this.setState({ facing: { x: -y, y: x } });
+          this.setState({
+            facing: { x: -y, y: x },
+            actions: [...this.state.actions, "LEFT"]
+          });
         } else if (command === "RIGHT") {
-          console.log("RIGHT");
           const x = this.state.facing.x;
           const y = this.state.facing.y;
-          this.setState({ facing: { x: y, y: -x } });
+          this.setState({
+            facing: { x: y, y: -x },
+            actions: [...this.state.actions, "RIGHT"]
+          });
         } else if (command === "REPORT") {
           // Need to print to screen somewhere
           const location = this.state.location;
-          console.log(
-            `Output: ${location.x},${location.y},${
-              direction.x[this.state.facing.x.toString()].y[
-                this.state.facing.y.toString()
-              ]
-            }`
-          );
-          this.setState({
-            location: null,
-            facing: { x: 0, y: 0 },
-            place: false
-          });
-          console.log("REPORT");
+          var report = `Output: ${location.x},${location.y},${
+            direction.x[this.state.facing.x.toString()].y[
+              this.state.facing.y.toString()
+            ]
+          }`;
+          this.setState({ actions: [...this.state.actions, report] });
         }
       }
       this.setState({ value: "" });
@@ -132,13 +133,43 @@ class App extends Component {
   }
 
   render() {
+    const actions = this.state.actions.map((elem, index) => (
+      <ActionView key={index} value={elem} />
+    ));
     return (
-      <div>
-        <input
-          onKeyPress={this.handlePress}
-          onChange={this.handleChange}
-          value={this.state.value}
-        />
+      <div className="container my-2">
+        <div className="row">
+          <div className="col-6 offset-3">
+            <div className="input-group">
+              <input
+                onKeyPress={this.handlePress}
+                onChange={this.handleChange}
+                value={this.state.value}
+                className="form-control"
+                placeholder="Enter move here..."
+              />
+              {this.state.placed ? (
+                <button
+                  onClick={this.handleReset}
+                  className="btn btn-danger btn-block mt-2"
+                >
+                  Reset
+                </button>
+              ) : (
+                ""
+              )}
+            </div>
+          </div>
+        </div>
+        {this.state.placed ? (
+          <div className="row mt-2">
+            <div className="col-6 offset-3">
+              <ul className="list-group">{actions}</ul>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
---
+++ src/components/ActionView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActionView = props => {
+  return <li className="list-group-item">{props.value}</li>;
+};
+
+export default ActionView;
